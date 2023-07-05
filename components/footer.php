fe8c93dbbe99d968911d0cdc261178e5f42f082e
jesuismaire <mari.nazaryan7173@gmail.com>
2023-07-06 00:31:25 +0400

    web

---
+++ components/footer.php
@@ -0,0 +1,24 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="tel:1111222333444"><i class="fas fa-phone"></i>+374(10) 000 000</a>
+         <a href="mailto:memuris_armenia@gmail.com"><i class="fas fa-envelope"></i> memuris_armenia@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Armenia, Yerevan </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Sona , Diana, Rima</span></div>
+
+</footer>
